dac75e7fd1f2d23a3a123a4a73aaa6e6617246e8
Patrick <pkwu@berkeley.edu>
2018-04-25 11:28:38 -0700

    [Add] - Adds Basic Nav Bar Component

--- pat-ui/client/src/components/App.jsx
+++ pat-ui/client/src/components/App.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Nav from '../components/Nav/Bar.jsx';
 
 const App = () => {
   return (
     <BrowserRouter>
       <div>
         <Switch>
-          {/* Nav */}
+          <Route path='/' component={Nav}/>
         </Switch>
         <Switch>
           {/* Body */}
---
+++ pat-ui/client/src/components/Nav/Bar.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Bar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      //
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        HELLO FROM NAV BAR
+      </div>
+    )
+  }
+}
+
+export default Bar;
