f97860fb2d2738beb1d78bc18d3f33d6e30333dd
Patrick <pkwu@berkeley.edu>
2018-04-25 11:43:47 -0700

    [Add] - Adds basic Body component

---
+++ pat-ui/client/src/components/Body/Main.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  state = {
+    //
+  }
+
+  render() {
+    return (
+      <div>
+        HELLO FROM MAIN
+      </div>
+    )
+  }
+}
