d583947dd06eacdad15fb2b1dcb7df58b2e2d68e
Patrick <pkwu@berkeley.edu>
2018-05-08 23:08:58 -0700

    [Add] - Adds proper positioning of git icon

--- pat-ui/client/src/components/Body/Projects/Entry.jsx
+++ pat-ui/client/src/components/Body/Projects/Entry.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
 const Entry = (prop) => (
-  <div>
+  <div class="container">
     <div class="project-entry">
       Project Name: {prop.name}
       <br/>
       Project Description: {prop.description}
       <br/>
-        Project Link: <a href={prop.url}>{prop.url}</a>
+    </div>
+    <div class="project-url">
+      <a href={prop.url}>
+        <img class="git-photo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQO4Ko_OfhGZrbubWBTYMtVhdcSmVQmWAgrNHe56lKxuyCBO9eWA"/>
+      </a>
     </div>
   </div>
 )
