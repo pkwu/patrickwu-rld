3a8c47cd71d6785d712ff7712584bd7e18eef68c
Patrick <pkwu@berkeley.edu>
2018-05-08 23:38:50 -0700

    [Fix] - Fixing banner component to auto-scale

--- pat-ui/client/src/components/Banner/index.jsx
+++ pat-ui/client/src/components/Banner/index.jsx
@@ -7,8 +7,9 @@ class Banner extends Component {
 
   render() {
     return (
-      // <div class="col-3">
-      <img class="wrapper" src="https://s-media-cache-ak0.pinimg.com/originals/72/75/e2/7275e29c611ff03a5fbacbca248d01c1.jpg"/>
+      <div class="wrapper">
+        <img class="banner"/>
+      </div>
       // </div>
     )
   }
