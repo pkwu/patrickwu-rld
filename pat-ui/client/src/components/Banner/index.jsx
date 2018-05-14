7e3ead4e5e412789c496b7eb428d5a3bd070893a
Patrick <pkwu@berkeley.edu>
2018-05-14 16:06:12 -0700

    [Fix] - Fixes nav bar to remain fixed over banner with z-index

--- pat-ui/client/src/components/Banner/index.jsx
+++ pat-ui/client/src/components/Banner/index.jsx
@@ -12,7 +12,6 @@ class Banner extends Component {
           https://c.wallhere.com/photos/b5/01/1680x977_px_abstract_wallpaper_art_images_asian_art_colourful_paints_cool_photos_hd_desktop_images_hd_photos-801830.jpg!d
 
           "/> */}
-        <div class="banner"/>
         <div class="banner-text">
           PATRICK WU'RLD
         </div>
