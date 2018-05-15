1ee76e2e0010bc60b1e439a6fab9e8fc3f7516c3
Patrick <pkwu@berkeley.edu>
2018-05-15 16:50:46 -0700

    [Add] - Adds parallax background

--- pat-ui/client/src/components/Banner/index.jsx
+++ pat-ui/client/src/components/Banner/index.jsx
@@ -8,12 +8,10 @@ class Banner extends Component {
   render() {
     return (
       <div class="banner-container">
-        {/* <img class="banner" src="
-          https://c.wallhere.com/photos/b5/01/1680x977_px_abstract_wallpaper_art_images_asian_art_colourful_paints_cool_photos_hd_desktop_images_hd_photos-801830.jpg!d
-
-          "/> */}
-        <div class="banner-text">
-          PATRICK WU'RLD
+        <div class="banner-image">
+          <div class="banner-text">
+            PATRICK WU'RLD
+          </div>
         </div>
       </div>
     )
